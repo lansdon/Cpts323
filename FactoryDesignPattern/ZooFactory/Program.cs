963c994c160c41965c055a7a7508f5f030e1abfe
unknown <brian.lamarche@gmail.com>
2013-02-12 00:17:04 -0800

    Added a factory design pattern model and example code to the repository

---
+++ FactoryDesignPattern/ZooFactory/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooFactory
+{
+    /// <summary>
+    /// A simple example of an animal
+    /// </summary>
+    public abstract class Animal
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public void Feed()
+        {
+        }
+    }
+
+    public sealed class Dog : Animal
+    {
+        public Dog()
+        {
+            Name = "Doggy";
+        }
+        public Dog(string doggyName)
+        {
+            Name = doggyName;
+        }
+    }
+    public sealed class Cat : Animal
+    {
+        public Cat()
+        {
+            Name = "Kibbles";
+        }
+    }
+
+    /// <summary>
+    /// Concrete factory that creates animals.
+    /// </summary>
+    public class ZooFactory
+    {
+        /// <summary>
+        /// Creates an animal.
+        /// </summary>
+        /// <returns></returns>
+        public Animal CreateAnimal()
+        {
+            Animal animal   = null;
+            animal          = new Dog();
+            return animal;
+        }            
+        /// <summary>
+        /// Creates a new animal
+        /// </summary>
+        /// <param name="animalType">Animal to create</param>
+        /// <returns>An animal.</returns>
+        public Animal CreateAnimal(AnimalType animalType)
+        {
+            Animal animal = null;
+
+            switch (animalType)
+            {
+                case AnimalType.Dog:
+                    animal = new Dog();
+                    break;
+                case AnimalType.Cat:
+                    animal = new Cat();
+                    break;
+                default:
+                    break;
+            }
+            return animal;
+        }
+        /// <summary>
+        /// Creates an animal based on the name.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Animal CreateDog(string name)
+        {
+            return new Dog(name);
+        }
+    }
+    /// <summary>
+    /// Simple enumeration for animal types.
+    /// </summary>
+    public enum AnimalType
+    {
+        Dog,
+        Cat
+    }
+        
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
