8c7653545d6df71ceddcdd69614bb3335db053d8
unknown <brian.lamarche@gmail.com>
2013-03-21 18:16:57 -0700

    added the observer pattern

---
+++ Examples/ModelingObserver/EventsExample/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace EventsExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCat = new System.Windows.Forms.Button();
+            this.catsLabel = new System.Windows.Forms.Label();
+            this.buttonDog = new System.Windows.Forms.Button();
+            this.buttonCow = new System.Windows.Forms.Button();
+            this.dogLabel = new System.Windows.Forms.Label();
+            this.cowLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCat
+            // 
+            this.buttonCat.Location = new System.Drawing.Point(12, 12);
+            this.buttonCat.Name = "buttonCat";
+            this.buttonCat.Size = new System.Drawing.Size(108, 65);
+            this.buttonCat.TabIndex = 0;
+            this.buttonCat.Text = "Add Cat";
+            this.buttonCat.UseVisualStyleBackColor = true;
+            this.buttonCat.Click += new System.EventHandler(this.buttonCat_Click);
+            // 
+            // catsLabel
+            // 
+            this.catsLabel.AutoSize = true;
+            this.catsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.catsLabel.Location = new System.Drawing.Point(178, 38);
+            this.catsLabel.Name = "catsLabel";
+            this.catsLabel.Size = new System.Drawing.Size(71, 31);
+            this.catsLabel.TabIndex = 1;
+            this.catsLabel.Text = "Cats";
+            // 
+            // buttonDog
+            // 
+            this.buttonDog.Location = new System.Drawing.Point(12, 92);
+            this.buttonDog.Name = "buttonDog";
+            this.buttonDog.Size = new System.Drawing.Size(108, 65);
+            this.buttonDog.TabIndex = 2;
+            this.buttonDog.Text = "Add Dog";
+            this.buttonDog.UseVisualStyleBackColor = true;
+            this.buttonDog.Click += new System.EventHandler(this.buttonDog_Click);
+            // 
+            // buttonCow
+            // 
+            this.buttonCow.Location = new System.Drawing.Point(12, 174);
+            this.buttonCow.Name = "buttonCow";
+            this.buttonCow.Size = new System.Drawing.Size(108, 65);
+            this.buttonCow.TabIndex = 3;
+            this.buttonCow.Text = "Add Cow";
+            this.buttonCow.UseVisualStyleBackColor = true;
+            this.buttonCow.Click += new System.EventHandler(this.buttonCow_Click);
+            // 
+            // dogLabel
+            // 
+            this.dogLabel.AutoSize = true;
+            this.dogLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dogLabel.Location = new System.Drawing.Point(178, 109);
+            this.dogLabel.Name = "dogLabel";
+            this.dogLabel.Size = new System.Drawing.Size(78, 31);
+            this.dogLabel.TabIndex = 4;
+            this.dogLabel.Text = "Dogs";
+            // 
+            // cowLabel
+            // 
+            this.cowLabel.AutoSize = true;
+            this.cowLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cowLabel.Location = new System.Drawing.Point(178, 187);
+            this.cowLabel.Name = "cowLabel";
+            this.cowLabel.Size = new System.Drawing.Size(83, 31);
+            this.cowLabel.TabIndex = 5;
+            this.cowLabel.Text = "Cows";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 255);
+            this.Controls.Add(this.cowLabel);
+            this.Controls.Add(this.dogLabel);
+            this.Controls.Add(this.buttonCow);
+            this.Controls.Add(this.buttonDog);
+            this.Controls.Add(this.catsLabel);
+            this.Controls.Add(this.buttonCat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCat;
+        private System.Windows.Forms.Label catsLabel;
+        private System.Windows.Forms.Button buttonDog;
+        private System.Windows.Forms.Button buttonCow;
+        private System.Windows.Forms.Label dogLabel;
+        private System.Windows.Forms.Label cowLabel;
+    }
+}
+
---
+++ Examples/ModelingObserver/EventsExample/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventsExample
+{
+    public partial class Form1 : Form
+    {
+        ZooManager m_manager;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            m_manager = new ZooManager();
+
+            m_manager.AddedCat += m_manager_AddedCat;
+            m_manager.AddedCow += m_manager_AddedCow;
+            m_manager.AddedDog += m_manager_AddedDog;
+        }
+
+        void m_manager_AddedDog(object sender, Animal animal)
+        {
+            dogLabel.Text = animal.Name;
+        }
+
+        void m_manager_AddedCow(object sender, Animal animal)
+        {
+            cowLabel.Text = animal.Name;
+        }
+
+        void m_manager_AddedCat(object sender, Animal animal)
+        {
+            catsLabel.Text = animal.Name;
+        }
+
+        private void buttonCat_Click(object sender, EventArgs e)
+        {
+            m_manager.AddCat(AnimalFactory.CreateAnimal(AnimalType.Cat));
+        }
+
+        private void buttonDog_Click(object sender, EventArgs e)
+        {
+            m_manager.AddDog(AnimalFactory.CreateAnimal(AnimalType.Dog));
+        }
+
+        private void buttonCow_Click(object sender, EventArgs e)
+        {
+            m_manager.AddCow(AnimalFactory.CreateAnimal(AnimalType.Cow));
+        }
+        
+    }
+}
