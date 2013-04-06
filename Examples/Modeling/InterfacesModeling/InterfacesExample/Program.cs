fb170317a79a9f5fe8d71fb8f4ac02afd4871a62
unknown <brian.lamarche@gmail.com>
2013-04-05 23:11:54 -0700

    updated modeling diagrams

---
+++ Examples/Modeling/InterfacesModeling/InterfacesExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExample
+{
+    /// <summary>
+    /// Interface example
+    /// </summary>
+    public interface IMissileLauncher
+    {
+        void FireMissile();
+        void Turn(double angle);
+        void TurnUp(double angle);
+    }
+
+    /// <summary>
+    /// Dummy class for pretending that the below concrete class realizes the IMissileLauncher interface.
+    /// </summary>
+    public class DreamCheekyMissileLauncher: IMissileLauncher
+    {
+
+        public void FireMissile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Turn(double angle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurnUp(double angle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
