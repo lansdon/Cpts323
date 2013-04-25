e3adfd1ebc4069697c4cae90926932ae0e196b1c
unknown <brian.lamarche@gmail.com>
2013-04-24 23:40:55 -0700

    target validation

---
+++ ProjectSDK/ASMLEngineSdk/ITargetValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMLEngineSdk
+{
+    /// <summary>
+    /// Interface for validating that a target was hit.
+    /// </summary>
+    public interface ITargetValidator
+    {
+        /// <summary>
+        /// Starts listening for any potential validations.
+        /// </summary>
+        void Start();
+        /// <summary>
+        /// Stops listening for any potential validations.
+        /// </summary>
+        void Stop();
+        /// <summary>
+        /// Resets the underlying validation scheme.
+        /// </summary>
+        void Reset();
+        /// <summary>
+        /// Retrieves data to see if a target was hit.
+        /// </summary>
+        /// <param name="name"></param>
+        bool WasTargetHit(string name);
+    }
+}
